e1d9c50e648821b0e146e25bc3fb57e0fb7d0de1
Iryna <irakogut23190@gmail.com>
2024-12-23 17:29:47 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write code here
+const appendRow = document.querySelector('.append-row');
+const removeRow = document.querySelector('.remove-row');
+const appendColumn = document.querySelector('.append-column');
+const removeColumn = document.querySelector('.remove-column');
+
+const table = document.querySelector('tbody');
+
+appendColumn.addEventListener('click', () => {
+  if ([...table.rows[0].cells].length < 10) {
+    [...table.rows].forEach((row) => {
+      const cloneColumn = document.createElement('td');
+
+      row.append(cloneColumn);
+    });
+  }
+});
+
+removeColumn.addEventListener('click', () => {
+  if ([...table.rows[0].cells].length > 2) {
+    [...table.rows].forEach((row) => {
+      row.deleteCell(table.rows[0].cells.length - 1);
+    });
+  }
+});
+
+appendRow.addEventListener('click', () => {
+  if ([...table.rows].length < 10) {
+    table.append(table.rows[0].cloneNode(true));
+  }
+});
+
+removeRow.addEventListener('click', () => {
+  if ([...table.rows].length > 2) {
+    table.deleteRow(table.rows[0]);
+  }
+});
