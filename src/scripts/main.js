f4f87ae2c0b2100da198c3d843ed238f7cde1449
Iryna <irakogut23190@gmail.com>
2024-12-30 18:26:00 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,8 +7,11 @@ const removeColumn = document.querySelector('.remove-column');
 
 const table = document.querySelector('tbody');
 
+const maxLength = 10;
+const minLength = 2;
+
 appendColumn.addEventListener('click', () => {
-  if ([...table.rows[0].cells].length < 10) {
+  if ([...table.rows[0].cells].length < maxLength) {
     appendColumn.setAttribute('enabled', true);
 
     [...table.rows].forEach((row) => {
@@ -18,55 +21,55 @@ appendColumn.addEventListener('click', () => {
     });
   }
 
-  if ([...table.rows[0].cells].length >= 10) {
+  if ([...table.rows[0].cells].length >= maxLength) {
     appendColumn.setAttribute('disabled', true);
   }
 
-  if ([...table.rows[0].cells].length > 2) {
+  if ([...table.rows[0].cells].length > minLength) {
     removeColumn.removeAttribute('disabled', true);
   }
 });
 
 removeColumn.addEventListener('click', () => {
-  if ([...table.rows[0].cells].length > 2) {
+  if ([...table.rows[0].cells].length > minLength) {
     [...table.rows].forEach((row) => {
       row.deleteCell(table.rows[0].cells.length - 1);
     });
   }
 
-  if ([...table.rows[0].cells].length <= 2) {
+  if ([...table.rows[0].cells].length <= minLength) {
     removeColumn.setAttribute('disabled', true);
   }
 
-  if ([...table.rows[0].cells].length < 10) {
+  if ([...table.rows[0].cells].length < maxLength) {
     appendColumn.removeAttribute('disabled', true);
   }
 });
 
 appendRow.addEventListener('click', () => {
-  if ([...table.rows].length < 10) {
+  if ([...table.rows].length < maxLength) {
     table.append(table.rows[0].cloneNode(true));
   }
 
-  if ([...table.rows].length >= 10) {
+  if ([...table.rows].length >= maxLength) {
     appendRow.setAttribute('disabled', true);
   }
 
-  if ([...table.rows].length > 2) {
+  if ([...table.rows].length > minLength) {
     removeRow.removeAttribute('disabled', true);
   }
 });
 
 removeRow.addEventListener('click', () => {
-  if ([...table.rows].length > 2) {
+  if ([...table.rows].length > minLength) {
     table.deleteRow(table.rows[0]);
   }
 
-  if ([...table.rows].length < 10) {
+  if ([...table.rows].length < maxLength) {
     appendRow.removeAttribute('disabled', true);
   }
 
-  if ([...table.rows].length <= 2) {
+  if ([...table.rows].length <= minLength) {
     removeRow.setAttribute('disabled', true);
   }
 });
